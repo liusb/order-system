ef18dd26a8d25116e53b54341cc095527902a691
liusb <liushaobo5@gmail.com>
2016-07-20 21:13:24 +0800

    add thread safe buffer

---
+++ src/main/java/com/alibaba/middleware/race/cache/SafeData.java
@@ -0,0 +1,14 @@
+package com.alibaba.middleware.race.cache;
+
+import com.alibaba.middleware.race.store.Data;
+
+public class SafeData {
+
+    private static final ThreadLocal<Data> data = new ThreadLocal<Data>() {
+        protected Data initialValue() {
+            Data data = new Data(new byte[1<<20]);
+            return data;
+        }
+    };
+
+}
