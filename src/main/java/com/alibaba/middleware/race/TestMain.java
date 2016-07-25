9f6dade00ff68b883af6f0e9250607530a4fbf3b
liusb <liushaobo5@gmail.com>
2016-07-25 14:41:44 +0800

    add System Check

--- src/main/java/com/alibaba/middleware/race/TestMain.java
+++ src/main/java/com/alibaba/middleware/race/TestMain.java
@@ -1,9 +1,14 @@
 package com.alibaba.middleware.race;
 
+import com.alibaba.middleware.race.store.PageStore;
+import com.alibaba.middleware.race.table.BuyerTable;
+import com.alibaba.middleware.race.table.GoodTable;
+import com.alibaba.middleware.race.table.HashTable;
+import com.alibaba.middleware.race.table.OrderTable;
+import com.alibaba.middleware.race.worker.LineReader;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class TestMain {
 
@@ -19,14 +24,24 @@ public class TestMain {
         orderFiles.add("./order_records.txt");
         buyerFiles.add("./buyer_records.txt");
         goodFiles.add("./good_records.txt");
+//        orderFiles.add("./prerun_data/order.0.0");
+//        orderFiles.add("./prerun_data/order.0.3");
+//        orderFiles.add("./prerun_data/order.1.1");
+//        orderFiles.add("./prerun_data/order.2.2");
+//        buyerFiles.add("./prerun_data/buyer.0.0");
+//        buyerFiles.add("./prerun_data/buyer.1.1");
+//        goodFiles.add("./prerun_data/good.0.0");
+//        goodFiles.add("./prerun_data/good.1.1");
+//        goodFiles.add("./prerun_data/good.2.2");
         storeFolders.add("./1");
         storeFolders.add("./2");
         storeFolders.add("./3");
 
         OrderSystem os = new OrderSystemImpl();
-        long beginTime = System.currentTimeMillis();
         os.construct(orderFiles, buyerFiles, goodFiles, storeFolders);
-        System.out.println("============>" + (System.currentTimeMillis() - beginTime));
+
+        // 检查构建是否有错误
+        systemCheck(orderFiles, buyerFiles, goodFiles, os);
 
         // 用例
         long orderid = 2982388;
@@ -88,4 +103,102 @@ public class TestMain {
             System.out.println("由于该字段不存在，返回值是null");
         }
     }
+
+    private static void systemCheck(Collection<String> orderFiles,
+                                    Collection<String> buyerFiles,
+                                    Collection<String> goodFiles,
+                                    OrderSystem os) {
+        long totalCount = 0;
+        for (PageStore pageFile: GoodTable.getInstance().baseTable.getPageFiles()) {
+            totalCount += pageFile.FileCheck();
+        }
+        System.out.println("========================total Count:" + totalCount);
+        totalCount = 0;
+        for (PageStore pageFile: BuyerTable.getInstance().baseTable.getPageFiles()) {
+            totalCount += pageFile.FileCheck();
+        }
+        System.out.println("========================total Count:" + totalCount);
+        totalCount = 0;
+        for (PageStore pageFile: OrderTable.getInstance().baseTable.getPageFiles()) {
+            totalCount += pageFile.FileCheck();
+        }
+        System.out.println("========================total Count:" + totalCount);
+
+        checkOrder(orderFiles, os);
+        checkHashTable(goodFiles, GoodTable.getInstance().baseTable, "goodid");
+        checkHashTable(buyerFiles, BuyerTable.getInstance().baseTable, "buyerid");
+    }
+
+    private static void checkOrder(Collection<String> orderFiles, OrderSystem os) {
+        for (String file: orderFiles) {
+            LineReader lineReader = new LineReader(file);
+            String line;
+            HashMap<String, String> raw = new HashMap<String, String>();
+            OrderSystem.Result result;
+            int lineCount = 0;
+            while (true) {
+                line = lineReader.nextLine();
+                if (line == null) {
+                    break;
+                }
+                raw.clear();
+                String[] kvs = line.split("\t");
+                for (String rawkv : kvs) {
+                    int p = rawkv.indexOf(':');
+                    String key = rawkv.substring(0, p);
+                    String value = rawkv.substring(p + 1);
+                    if (key.length() == 0 || value.length() == 0) {
+                        throw new RuntimeException("Bad data:" + line);
+                    }
+                    raw.put(key, value);
+                }
+                result = os.queryOrder(Long.parseLong(raw.get("orderid")), null);
+                for (Map.Entry<String, String> entry: raw.entrySet()) {
+                    if (!result.get(entry.getKey()).valueAsString().equals(entry.getValue())) {
+                        throw new RuntimeException("竟然不相等, raw:" + entry.getValue()
+                                + ", find:" + result.get(entry.getKey()).valueAsString());
+                    }
+                }
+                lineCount++;
+            }
+            System.out.println(file + " checked. total count: " +lineCount);
+        }
+    }
+
+    private static void checkHashTable(Collection<String> rawFiles, HashTable table, String keyStr) {
+        for (String file: rawFiles) {
+            LineReader lineReader = new LineReader(file);
+            String line;
+            HashMap<String, String> raw = new HashMap<String, String>();
+            HashMap<String, Object> result;
+            int lineCount = 0;
+            while (true) {
+                line = lineReader.nextLine();
+                if (line == null) {
+                    break;
+                }
+                raw.clear();
+                String[] kvs = line.split("\t");
+                for (String rawkv : kvs) {
+                    int p = rawkv.indexOf(':');
+                    String key = rawkv.substring(0, p);
+                    String value = rawkv.substring(p + 1);
+                    if (key.length() == 0 || value.length() == 0) {
+                        throw new RuntimeException("Bad data:" + line);
+                    }
+                    raw.put(key, value);
+                }
+                result = table.findRecord(raw.get(keyStr));
+                for (Map.Entry<String, String> entry: raw.entrySet()) {
+                    if (!result.get(entry.getKey()).toString().equals(entry.getValue())) {
+                        throw new RuntimeException("竟然不相等, raw:" + entry.getValue()
+                                + ", find:" + result.get(entry.getKey()).toString());
+                    }
+                }
+                lineCount++;
+            }
+            System.out.println(file + " checked. total count: " +lineCount);
+        }
+    }
+
 }
