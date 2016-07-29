e2024283488dc66158dfdce03329e9ecfb3c958a
liusb <liushaobo5@gmail.com>
2016-07-29 17:20:15 +0800

    将读操作修改为256KB的wraped buffer读入

--- src/main/java/com/alibaba/middleware/race/worker/OrderReader.java
+++ src/main/java/com/alibaba/middleware/race/worker/OrderReader.java
@@ -4,7 +4,7 @@ import com.alibaba.middleware.race.index.RecordIndex;
 import com.alibaba.middleware.race.table.OrderLine;
 
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -29,59 +29,60 @@ public class OrderReader implements Runnable {
         int i;
         int outSize = outs.size();
         long lineCount = 0;
-        final int B_SIZE = 8*1024;
-        final long MAP_BUFFER_SIZE = 1<<30;
+        final int B_SIZE = 256*1024;
+        final byte[] bArray = new byte[B_SIZE+2048];
+        int maxLineSize = 0;
         try {
             for (Map.Entry<String, Byte> entry : this.files.entrySet()) {
                 FileChannel fileChannel = FileChannel.open(Paths.get(entry.getKey()));
-                long fileSize = fileChannel.size();
                 byte fileId = entry.getValue();
                 long nextLineOffset = 0;
+
                 int lineBegin = 0;
-                byte[] bArray = new byte[B_SIZE];
+                int lineLength = 0;
                 int bArrayOffset = 0;
-                int mapTime = (int)((fileSize-1)/MAP_BUFFER_SIZE)+1;
-                for (int j=0; j < mapTime; j++) {
-                    long mapBegin = (MAP_BUFFER_SIZE)*j;
-                    long mapSize = Math.min(MAP_BUFFER_SIZE, fileSize - mapBegin);
-                    MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, mapBegin, mapSize);
-                    while (buffer.hasRemaining()) {
-                        int getSize = Math.min(buffer.remaining(), B_SIZE-bArrayOffset);
-                        buffer.get(bArray, bArrayOffset, getSize);
-                        int bArrayDataSize = bArrayOffset + getSize;
-                        for ( ; bArrayOffset < bArrayDataSize; bArrayOffset++) {
-                            if (bArray[bArrayOffset]=='\n') {
-                                OrderLine orderLine = new OrderLine(new RecordIndex(fileId, nextLineOffset),
-                                        new String(bArray, lineBegin, bArrayOffset-lineBegin));
-                                nextLineOffset += (bArrayOffset-lineBegin+1);
-                                lineBegin = bArrayOffset+1;
-                                while (true) {
-                                    try {
-                                        for (i = 1; i < 16; i++) {
-                                            if (outs.get((int) (lineCount + i * step) % outSize)
-                                                    .offer(orderLine, 420, TimeUnit.MICROSECONDS)) {
-                                                break;
-                                            }
-                                        }
-                                        if (i == 16) {
-                                            outs.get((int) (lineCount + threadId) % outSize).put(orderLine);
-                                        }
-                                        break;
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                lineCount++;
+                int getSize;
+                int bArrayDataSize;
+                ByteBuffer warpedBuffer = ByteBuffer.wrap(bArray, bArrayOffset, B_SIZE);
+                while ((getSize = fileChannel.read(warpedBuffer)) != -1) {
+                    for (bArrayDataSize = bArrayOffset + getSize; bArrayOffset < bArrayDataSize; bArrayOffset++) {
+                        if (bArray[bArrayOffset]=='\n') {
+                            lineLength = bArrayOffset-lineBegin;
+                            OrderLine orderLine = new OrderLine(new RecordIndex(fileId, nextLineOffset),
+                                    new String(bArray, lineBegin, lineLength));
+                            if (maxLineSize < lineLength) {
+                                maxLineSize = lineLength;
                             }
+                            nextLineOffset += (lineLength+1);
+                            lineBegin = bArrayOffset+1;
+                            while (true) {
+                                try {
+                                    for (i = 1; i < 16; i++) {
+                                        if (outs.get((int) (lineCount + i * step) % outSize)
+                                                .offer(orderLine, 420, TimeUnit.MICROSECONDS)) {
+                                            break;
+                                        }
+                                    }
+                                    if (i == 16) {
+                                        outs.get((int) (lineCount + threadId) % outSize).put(orderLine);
+                                    }
+                                    break;
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            lineCount++;
                         }
-                        bArrayOffset = bArrayOffset-lineBegin;
-                        System.arraycopy(bArray, lineBegin, bArray, 0, bArrayOffset);
-                        lineBegin = 0;
                     }
+                    bArrayOffset = bArrayDataSize-lineBegin;
+                    System.arraycopy(bArray, lineBegin, bArray, 0, bArrayOffset);
+                    lineBegin = 0;
+                    warpedBuffer = ByteBuffer.wrap(bArray, bArrayOffset, B_SIZE);
                 }
                 fileChannel.close();
             }
-            System.out.println("INFO: Reader thread completed. lineCount:" + lineCount + " Thread id:" + threadId);
+            System.out.println("INFO: Order Reader thread completed. lineCount:"
+                    + lineCount + " max line size: " + maxLineSize + " Thread id:" + threadId);
         } catch (IOException e) {
             e.printStackTrace();
         }
