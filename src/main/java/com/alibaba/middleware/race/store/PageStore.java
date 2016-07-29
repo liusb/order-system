ac63921744f2fcb9d8613dd71e528004edbcebf2
liusb <liushaobo5@gmail.com>
2016-07-29 17:23:32 +0800

    修改为积累到128KB时 再批量写入

--- src/main/java/com/alibaba/middleware/race/store/PageStore.java
+++ src/main/java/com/alibaba/middleware/race/store/PageStore.java
@@ -173,7 +173,7 @@ public class PageStore {
     }
 
     public void checkCache() {
-        if (pagesWaitToWrite.size() >= 16) {
+        if (pagesWaitToWrite.size() >= 32) {
             Collections.sort(pagesWaitToWrite);
             for (DataPage page: pagesWaitToWrite) {
                 this.aioWrite(page);
