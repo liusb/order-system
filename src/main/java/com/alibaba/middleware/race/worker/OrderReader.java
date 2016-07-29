54529611aff9c699c9663721967ba4ff88987706
liusb <liushaobo5@gmail.com>
2016-07-29 10:00:27 +0800

    修改读取buffer

--- src/main/java/com/alibaba/middleware/race/worker/OrderReader.java
+++ src/main/java/com/alibaba/middleware/race/worker/OrderReader.java
@@ -29,46 +29,54 @@ public class OrderReader implements Runnable {
         int i;
         int outSize = outs.size();
         long lineCount = 0;
+        final int B_SIZE = 8*1024;
+        final long MAP_BUFFER_SIZE = 1<<30;
         try {
             for (Map.Entry<String, Byte> entry : this.files.entrySet()) {
                 FileChannel fileChannel = FileChannel.open(Paths.get(entry.getKey()));
                 long fileSize = fileChannel.size();
                 byte fileId = entry.getValue();
-                byte[] strBuffer = new byte[1024];
-                int posInStrBuffer = 0;
-                int mapTime = (int)((fileSize-1)>>30)+1;
+                long nextLineOffset = 0;
+                int lineBegin = 0;
+                byte[] bArray = new byte[B_SIZE];
+                int bArrayOffset = 0;
+                int mapTime = (int)((fileSize-1)/MAP_BUFFER_SIZE)+1;
                 for (int j=0; j < mapTime; j++) {
-                    long mapBegin = ((long)(1<<30))*j;
-                    long mapSize = Math.min((1<<30), fileSize-mapBegin);
+                    long mapBegin = (MAP_BUFFER_SIZE)*j;
+                    long mapSize = Math.min(MAP_BUFFER_SIZE, fileSize - mapBegin);
                     MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, mapBegin, mapSize);
-                    String line;
-                    for (int posInMap = 0; posInMap < mapSize; posInMap++) {
-                        byte ch = buffer.get();
-                        if (ch == '\n') {
-                            line = new String(strBuffer, 0, posInStrBuffer);
-                            OrderLine orderLine = new OrderLine(new RecordIndex(fileId,
-                                    mapBegin+posInMap-posInStrBuffer), line);
-                            posInStrBuffer=0;  // 重置pos
-                            while (true) {
-                                try {
-                                    for (i = 1; i < 16; i++) {
-                                        if (outs.get((int) (lineCount + i * step) % outSize)
-                                                .offer(orderLine, 420, TimeUnit.MICROSECONDS)) {
-                                            break;
+                    while (buffer.hasRemaining()) {
+                        int getSize = Math.min(buffer.remaining(), B_SIZE-bArrayOffset);
+                        buffer.get(bArray, bArrayOffset, getSize);
+                        int bArrayDataSize = bArrayOffset + getSize;
+                        for ( ; bArrayOffset < bArrayDataSize; bArrayOffset++) {
+                            if (bArray[bArrayOffset]=='\n') {
+                                OrderLine orderLine = new OrderLine(new RecordIndex(fileId, nextLineOffset),
+                                        new String(bArray, lineBegin, bArrayOffset-lineBegin));
+                                nextLineOffset += (bArrayOffset-lineBegin+1);
+                                lineBegin = bArrayOffset+1;
+                                while (true) {
+                                    try {
+                                        for (i = 1; i < 16; i++) {
+                                            if (outs.get((int) (lineCount + i * step) % outSize)
+                                                    .offer(orderLine, 420, TimeUnit.MICROSECONDS)) {
+                                                break;
+                                            }
                                         }
+                                        if (i == 16) {
+                                            outs.get((int) (lineCount + threadId) % outSize).put(orderLine);
+                                        }
+                                        break;
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
                                     }
-                                    if (i == 16) {
-                                        outs.get((int) (lineCount + threadId) % outSize).put(orderLine);
-                                    }
-                                    break;
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
                                 }
+                                lineCount++;
                             }
-                            lineCount++;
-                        } else {
-                            strBuffer[posInStrBuffer++] = ch;
                         }
+                        bArrayOffset = bArrayOffset-lineBegin;
+                        System.arraycopy(bArray, lineBegin, bArray, 0, bArrayOffset);
+                        lineBegin = 0;
                     }
                 }
                 fileChannel.close();
