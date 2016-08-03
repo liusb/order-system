30c6101b709405a1d472ddd4b7b51125d856ec1e
liusb <liushaobo5@gmail.com>
2016-08-03 09:35:58 +0800

    加大线程池数量

--- src/main/java/com/alibaba/middleware/race/cache/ThreadPool.java
+++ src/main/java/com/alibaba/middleware/race/cache/ThreadPool.java
@@ -14,8 +14,8 @@ public class ThreadPool {
     public ThreadPoolExecutor pool;
 
     private ThreadPool() {
-         this.pool = new ThreadPoolExecutor(512, 512, 0L, TimeUnit.MILLISECONDS,
-                        new LinkedBlockingQueue<Runnable>(4096));
+         this.pool = new ThreadPoolExecutor(800, 800, 0L, TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<Runnable>(8192));
         pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
     }
 }
